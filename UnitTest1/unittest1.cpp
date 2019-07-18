ae379847d190f29dc051d69d91c56633d576023f
Demon <774280605@qq.com>
2019-07-18 17:16:34 +0800

    ...

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -11,9 +11,39 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
+			char buffer[4097] = { 0 };
+			memset(buffer, 'a', 4096);
 			MessageBlock block;
-			block.add("zhangxiaofei", 12);
-			Assert::AreEqual((long)12, block.length());
+			block.add(buffer, 4096);
+
+			Assert::AreEqual(4096L, block.length());
+
+			
+
+			char buffer2[4097] = { 0 };
+			block.remove(buffer2, 4096);
+			Assert::IsFalse(strcmp(buffer, buffer2));
+
+			char buffer3[36] = { 0 };
+			block.add("zhang", 5);
+			Assert::AreEqual(5L, block.length());
+
+			block.remove(buffer3, 5);
+			Assert::AreEqual(0L, block.length());
+
+			
+			char buffer4[8193] = {};
+			memset(buffer4, 'b', 8092);
+			block.add(buffer4, 8192);
+			Assert::AreEqual(8192L, block.length());
+			
+			char buffer5[8193] = {};
+			block.remove(buffer5,8192);
+			Assert::AreEqual(0L, block.length());
+
+
+
+
 		}
 
 	};
